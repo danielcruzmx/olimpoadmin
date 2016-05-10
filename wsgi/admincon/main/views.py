1262a9645e1265202e6af51b5e1812020187cdcc
danielcruzmx <danielcruzmx@hotmail.com>
2016-05-10 15:06:08 -0500

    base

--- wsgi/admincon/main/views.py
+++ wsgi/admincon/main/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
+from django.template import RequestContext
 
 @login_required()
 def home(request):
-     return render_to_response('home/home.html')
+     return render_to_response('home/home.html', context_instance=RequestContext(request))
