1d5c6221c9fac60e26b555987a59f7466a2a3fdd
danielcruzmx <danielcruzmx@hotmail.com>
2016-05-17 16:58:41 -0500

    ajuste static files

--- wsgi/admincon/admincon/settings.py
+++ wsgi/admincon/admincon/settings.py
@@ -128,11 +128,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(WSGI_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, '..', 'static')
+
 STATICFILES_DIRS = (
     os.path.join(WSGI_DIR,'static'),
 )
+
+STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
