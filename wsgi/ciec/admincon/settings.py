8ad24e46ba50c79ee71f9171ad17755f64a3ec4e
danielcruzmx <danielcruzmx@hotmail.com>
2017-10-06 20:34:32 -0500

    produccion
    cambios para deploy a “producción” local

---
+++ wsgi/ciec/admincon/settings.py
@@ -0,0 +1,166 @@
+"""
+Django settings for myproject project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.8/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.8/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+DJ_PROJECT_DIR = os.path.dirname(__file__)
+BASE_DIR = os.path.dirname(DJ_PROJECT_DIR)
+WSGI_DIR = os.path.dirname(BASE_DIR)
+REPO_DIR = os.path.dirname(WSGI_DIR)
+DATA_DIR = os.environ.get('OPENSHIFT_DATA_DIR', BASE_DIR)
+
+import sys
+sys.path.append(os.path.join(REPO_DIR, 'libs'))
+import secrets
+SECRETS = secrets.getter(os.path.join(DATA_DIR, 'secrets.json'))
+
+ON_OPENSHIFT = False
+if 'OPENSHIFT_DATA_DIR' in os.environ:
+    ON_OPENSHIFT = True
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = SECRETS['secret_key']
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+from socket import gethostname
+ALLOWED_HOSTS = [
+    gethostname(), # For internal OpenShift load balancer security purposes.
+    os.environ.get('OPENSHIFT_APP_DNS'), # Dynamically map to the OpenShift gear name.
+    #'example.com', # First DNS alias (set up in the app)
+    #'www.example.com', # Second DNS alias (set up in the app)
+]
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.admin',
+    'explorer',
+    'rest_framework',
+    'django_tables2',
+    'main',
+    'c_comonfort',
+    'c_olimpo',
+    'c_sadicarnot',
+    'c_tabachines',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
+)
+
+# GETTING-STARTED: change 'admincon' to your project name:
+ROOT_URLCONF = 'admincon.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR + '/templates/'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.core.context_processors.request',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'admincon.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
+
+if ON_OPENSHIFT:
+    DATABASES = {
+    	'default': {
+        	'ENGINE': 'django.db.backends.mysql',
+        	'NAME': os.environ['OPENSHIFT_APP_NAME'],
+        	'USER': os.environ['OPENSHIFT_MYSQL_DB_USERNAME'],
+        	'PASSWORD': os.environ['OPENSHIFT_MYSQL_DB_PASSWORD'],
+        	'HOST': os.environ['OPENSHIFT_MYSQL_DB_HOST'],
+        	'PORT': os.environ['OPENSHIFT_MYSQL_DB_PORT']
+    	}
+    }
+else:
+    DATABASES = {
+    	'default': {
+        	'ENGINE': 'django.db.backends.mysql',
+        	'NAME': 'administra',
+        	'USER': 'root',
+        	'PASSWORD': 'valeria1',
+        	'HOST': 'localhost',
+        	'PORT': '3306'
+    	}
+    }
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.8/topics/i18n/
+
+LANGUAGE_CODE = 'en-MX'
+
+TIME_ZONE = 'America/Mexico_City'
+
+USE_I18N = True
+
+#USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.8/howto/static-files/
+
+STATICFILES_DIRS = (
+    '/Library/Python/2.7/site-packages/django/contrib/admin/static/',
+)
+
+STATIC_ROOT = '/Users/danielcruzmx/olimpo/administra/wsgi/ciec/static/'
+
+STATIC_URL = '/static/'
+
+#ADMIN_MEDIA_PREFIX = '/static/admin/'
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+ )
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+}
+
--- wsgi/ciec/wsgi.py
+++ wsgi/ciec/wsgi.py
@@ -7,11 +7,16 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
 
 # GETTING-STARTED: change 'admincon' to your project name:
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "admincon.settings")
+#os.environ["DJANGO_SETTINGS_MODULE"] = "ciec.settings"
+
+sys.path.append('/Users/danielcruzmx/olimpo/administra/wsgi/ciec/admincon')
+sys.path.append('/Users/danielcruzmx/olimpo/administra/wsgi/ciec')
+
+os.environ["DJANGO_SETTINGS_MODULE"] = "admincon.settings"
 
 application = get_wsgi_application()
